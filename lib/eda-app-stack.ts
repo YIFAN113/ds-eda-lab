a01ff1089d4a19b9aaca8a58e17314a236d878c1
Yifan Gu <20095253@mail.wit.ie>
2024-04-15 22:10:16 +0800

    Completed Fan out implementation - added emailing logic

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -31,10 +31,14 @@ export class EDAAppStack extends cdk.Stack {
     const imageProcessQueue = new sqs.Queue(this, "img-created-queue", {
       receiveMessageWaitTime: cdk.Duration.seconds(10),
     });
+    const mailerQ = new sqs.Queue(this, "mailer-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
 
     const newImageTopic = new sns.Topic(this, "NewImageTopic", {
       displayName: "New Image topic",
     }); 
+
 
   // Lambda functions
 
@@ -49,6 +53,13 @@ export class EDAAppStack extends cdk.Stack {
     }
   );
 
+  const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+    runtime: lambda.Runtime.NODEJS_16_X,
+    memorySize: 1024,
+    timeout: cdk.Duration.seconds(3),
+    entry: `${__dirname}/../lambdas/mailer.ts`,
+  });
+
  // S3 --> SQS
  imagesBucket.addEventNotification(
   s3.EventType.OBJECT_CREATED,
@@ -57,6 +68,8 @@ export class EDAAppStack extends cdk.Stack {
 newImageTopic.addSubscription(
   new subs.SqsSubscription(imageProcessQueue)
 );
+newImageTopic.addSubscription(
+  new subs.SqsSubscription(mailerQ));
 
  // SQS --> Lambda
   const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
@@ -64,8 +77,25 @@ newImageTopic.addSubscription(
     maxBatchingWindow: cdk.Duration.seconds(10),
   });
 
-  processImageFn.addEventSource(newImageEventSource);
+  const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+    batchSize: 5,
+    maxBatchingWindow: cdk.Duration.seconds(10),
+  }); 
 
+  processImageFn.addEventSource(newImageEventSource);
+  mailerFn.addEventSource(newImageMailEventSource);
+
+  mailerFn.addToRolePolicy(
+    new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "ses:SendEmail",
+        "ses:SendRawEmail",
+        "ses:SendTemplatedEmail",
+      ],
+      resources: ["*"],
+    })
+  );
   // Permissions
 
   imagesBucket.grantRead(processImageFn);
